9faac336cb1c24c2834c143a5eb063fb5a408e0b
Emre Ergun <emreergun@engramsoft.com>
2024-12-09 14:40:43 +0300

    push notifications by expo added to mobile app, and user in the supabase updated

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,36 +4,41 @@ import { ToastProvider } from 'react-native-toast-notifications';
 import AuthProvider from '../providers/auth-provider';
 import { QueryProvider } from '../providers/query-provider';
 import { StripeProvider } from '@stripe/stripe-react-native';
+import NotificationProvider from '../providers/notification-provider';
 
 const RootLayout = () => {
   return (
     <ToastProvider>
       <AuthProvider>
         <QueryProvider>
-          <StripeProvider publishableKey={process.env.EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY!}>
-            <Stack>
-              <Stack.Screen
-                name='(shop)'
-                options={{ headerShown: false, title: 'Shop' }}
-              />
-              <Stack.Screen
-                name='categories'
-                options={{ headerShown: false, title: 'Categories' }}
-              />
-              <Stack.Screen
-                name='product'
-                options={{ headerShown: false, title: 'Product' }}
-              />
-              <Stack.Screen
-                name='cart'
-                options={{ presentation: 'modal', title: 'Shopping Cart' }}
-              />
-              <Stack.Screen
-                name='auth'
-                options={{ headerShown: false, title: 'Authenticate' }}
-              />
-            </Stack>
-            <StatusBar style='auto' />
+          <StripeProvider
+            publishableKey={process.env.EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY!}
+          >
+            <NotificationProvider>
+              <Stack>
+                <Stack.Screen
+                  name='(shop)'
+                  options={{ headerShown: false, title: 'Shop' }}
+                />
+                <Stack.Screen
+                  name='categories'
+                  options={{ headerShown: false, title: 'Categories' }}
+                />
+                <Stack.Screen
+                  name='product'
+                  options={{ headerShown: false, title: 'Product' }}
+                />
+                <Stack.Screen
+                  name='cart'
+                  options={{ presentation: 'modal', title: 'Shopping Cart' }}
+                />
+                <Stack.Screen
+                  name='auth'
+                  options={{ headerShown: false, title: 'Authenticate' }}
+                />
+              </Stack>
+              <StatusBar style='auto' />
+            </NotificationProvider>
           </StripeProvider>
         </QueryProvider>
       </AuthProvider>
